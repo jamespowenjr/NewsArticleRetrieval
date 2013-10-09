efd197a211e2617b4d701a46c0f7a8704556ed3c
jamespowenjr <jamespowenjr@gmail.com>
2013-10-08 23:17:57 -0400

    * Initial import

---
+++ NYTimesAPI/src/main/java/com/bah/finance/APIClient.java
@@ -0,0 +1,52 @@
+package com.bah.finance;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLEncoder;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class APIClient {
+
+	public static final String baseURL = "http://api.nytimes.com/svc/search/v2/articlesearch";
+	public static final String extension = ".json";
+	public static final String key = "api-key=6251375db21329deb5245573d9b0ea77:1:68230793";
+	public static final String query = "q=";
+	public static final String source = URLEncoder.encode("fq=source:(\"The New York Times\")");
+	public static final String startDate = "begin_date=20130101";
+	public static final String endDate = "end_date=20131007";
+
+	public static void main(String args[]) throws ClientProtocolException,
+			IOException {
+
+		String url = baseURL + extension + "?";
+
+		url += key;
+		url += "&" + source;
+		url += "&" + query + URLEncoder.encode("\"crude oil\"");
+		url += "&" + startDate;
+		url += "&" + endDate;
+
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		System.out.println(url);
+		HttpGet get = new HttpGet(url);
+
+		HttpResponse response = httpClient.execute(get);
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				(response.getEntity().getContent())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		httpClient.getConnectionManager().shutdown();
+	}
+
+}
